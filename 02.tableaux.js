2931d49333b1cad2694ffa25e3139c657bfab81d
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2020-01-07 11:10:11 +0100

    TP2

--- 02.tableaux.js
+++ 02.tableaux.js
@@ -1,4 +1,37 @@
 var villes= ['nantes', 'paris', 'saint-nazaire', 'angers', 'le mans'];
-forEach(villes){
-    console.log();
+villes.forEach(function(ville){
+    console.log(ville);
+});
+
+var contientA = function(element, index, array){
+    return element.indexOf("a")>= 0;
 }
+
+var contientTiret = function(element, index, array){
+    return element.indexOf("-")>= 0;
+}
+
+var contientPasTiretEspace = function(element, index, array){
+    return element.indexOf("-") < 0 & element.indexOf(" ") < 0;
+}
+
+var derniereLettreS = function(element, index, array){
+    return element.endsWith("s");
+}
+
+var lettreADansToutesLesVilles = villes.every(contientA);
+console.log("lettreADansToutesLesVilles = "+lettreADansToutesLesVilles);
+
+var auMoinsUneVilleAvecUnTiret = villes.some(contientTiret);
+console.log("auMoinsUneVilleAvecUnTiret = "+auMoinsUneVilleAvecUnTiret);
+
+var villesSansTiretSansEspace = villes.filter(contientPasTiretEspace);
+villesSansTiretSansEspace.forEach(function(ville){
+    console.log(ville);
+})
+
+var villesMajusculeSeTerminantParS = villes.filter(derniereLettreS).map(function (element, index, array) { return element.toUpperCase(); });
+villesMajusculeSeTerminantParS.forEach(function(ville){
+    console.log(ville);
+})
+
